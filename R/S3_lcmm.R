3ff752adf26cf9b7aca5cff000ecdd52b700110e
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-10 21:24:28 -0500

    tzh

--- R/S3_lcmm.R
+++ R/S3_lcmm.R
@@ -103,7 +103,6 @@ nobsText.lcmm <- function(x) {
 
 #' @rdname S3_lcmm
 #' @importFrom methods new
-#' @importFrom utils bibentry
 #' @importClassesFrom fastmd md_lines
 #' @importFrom ecip desc_
 #' @export desc_.lcmm
@@ -111,18 +110,7 @@ nobsText.lcmm <- function(x) {
 desc_.lcmm <- function(x) {
   
   'latent class mixed-effect [@Proust06]' |>
-    new(Class = 'md_lines', bibentry = bibentry(
-      bibtype = 'article', key = 'Proust06',
-      author = 'Proust, C\u00e9cile and Jacqmin-Gadda, H\u00e9l\u00e8ne and Taylor, Jeremy M. G. and Ganiayre, Julien and Commenges, Daniel',
-      title = 'A Nonlinear Model with Latent Process for Cognitive Evolution Using Multivariate Longitudinal Data',
-      journal = 'Biometrics',
-      volume = '62',
-      number = '4',
-      pages = '1014-1024',
-      year = '2006',
-      month = '04',
-      doi = '10.1111/j.1541-0420.2006.00573.x'
-    ))
+    new(Class = 'md_lines', bibentry = .proust06())
   
 }
 
---
+++ R/bibs.R
@@ -0,0 +1,21 @@
+
+#' @title bibs of lcmm package
+#' 
+#' @keywords internal
+#' @importFrom utils bibentry
+#' @name lcmm_bib
+#' @export
+.proust06 <- \() {
+  bibentry(
+    bibtype = 'article', key = 'Proust06',
+    author = 'Proust, C\u00e9cile and Jacqmin-Gadda, H\u00e9l\u00e8ne and Taylor, Jeremy M. G. and Ganiayre, Julien and Commenges, Daniel',
+    title = 'A Nonlinear Model with Latent Process for Cognitive Evolution Using Multivariate Longitudinal Data',
+    journal = 'Biometrics',
+    volume = '62',
+    number = '4',
+    pages = '1014-1024',
+    year = '2006',
+    month = '04',
+    doi = '10.1111/j.1541-0420.2006.00573.x'
+  )
+}
