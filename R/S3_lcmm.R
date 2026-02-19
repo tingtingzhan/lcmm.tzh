43b392e342706ade7f902fc3fdf4d15abe6ea8e7
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-18 19:58:02 -0500

    tzh

--- R/S3_lcmm.R
+++ R/S3_lcmm.R
@@ -164,7 +164,7 @@ coef_.lcmm <- function(x) {
 #' list(
 #'  'model 1' = m20,
 #'  'model 2' = m21
-#' ) |> fastmd::render2html(file = 'lcmm')
+#' ) |> fastmd::render2html()
 #' @keywords internal
 #' @importFrom fastmd md_ md_autoplot_
 #' @importFrom ecip .md_reg
